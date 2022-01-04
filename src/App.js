a23e3ab7bd34610915b78f3860233c08ff718d8f
Alberto Isaza <isaz@hotmail.com>
2022-01-04 08:56:56 -0500

    complemento de rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './LogoDOKO.svg';
+//Importar dependencias
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Nav } from 'react-bootstrap'
+
+//Importar paginas
 import Auth from './pages/Auth';
 import CashRegister from './pages/CashRegister';
 import Product from './pages/Product';
 import Sell from './pages/Sell';
 import Stock from './pages/Stock';
 import User from './pages/User';
+
+//Importar Assets
+import logo from './LogoDOKO.svg';
 
 
 function App() {
@@ -15,13 +21,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+     
       <nav>
-        <Link to="/">Autenticacion </Link><br />
-        <Link to="/cash">Caja</Link><br />
-        <Link to="/product">Gestion de Productos</Link><br />
-        <Link to="/sell">Gestion de Ventas</Link><br />
-        <Link to="/stock">Gestion de Inventarios</Link><br />
-        <Link to="/user">Gestion de Usuarios</Link><br /><br />
+      | <Link to="/">Autenticacion</Link> | 
+        | <Link to="/cash">Caja</Link> |
+        | <Link to="/product">Gestion de Productos</Link> |
+        | <Link to="/sell">Gestion de Ventas</Link> |
+        | <Link to="/stock">Gestion de Inventarios</Link> |
+        | <Link to="/user">Gestion de Usuarios</Link> |<br /><br />
       </nav>
       <Routes>
         <Route path="/" element={<Auth />} />
