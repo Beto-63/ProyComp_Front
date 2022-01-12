3994fb1e7fac6ac96990185b2ca70dbd3e18689c
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-01-12 01:15:10 -0500

    agregados el useState y los handlers para ClientForm

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClientForm from '../components/clientes/ClientForm'
 
 const Client = () => {
+    //estado que va a almacenar en un arreglo los datos del ClientForm 
+    const [client, setClient] = useState([]);
+    //Manejador para el cliente
+    const handleClient = (objClient)=>{
+        let array = [...client, objClient]
+        setClient(array)
+    }
+    
     return (
         <div>
             <h2>Cliente</h2>
-            <ClientForm/>
+            {/*Se envía el handleClient como prop al formulario*/}
+            <ClientForm handleClient={handleClient}/>
         </div>
         
     )
