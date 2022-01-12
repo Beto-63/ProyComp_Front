6bd0988e4eadc62a5c7746b4528266c01f0f143e
Alberto Isaza <isaz@hotmail.com>
2022-01-11 20:23:53 -0500

    Botones y Canvas

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@
 
 //Importar Assets
 import './App.css';
+import BotonFondoClaro from './components/generic/BotonFondoClaro';
+import BotonFondoOscuro from './components/generic/BotonFondoOscuro';
+import CanvasClaro from './components/generic/CanvasClaro';
+import CanvasOscuro from './components/generic/CanvasOscuro';
 import logo from './LogoDOKO.svg';
 import { AppRouter } from './routers/AppRouter';
 
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
+      <CanvasClaro />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <AppRouter/>
+      <AppRouter/> */}
+      
     </div>
 
   );
