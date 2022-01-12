86e6501fffdc503981d1cfbd537ff94746de0509
Alberto Isaza <isaz@hotmail.com>
2022-01-12 18:24:39 -0500

    update

--- src/components/Stock/CreateStockItem.js
+++ src/components/Stock/CreateStockItem.js
@@ -35,6 +35,7 @@ const CreateStockItem = () => {
 
           <Form>
             <Field className="campo_entrada" placeholder="Nombre" name="name" type="text" />
+            <p className='error'>{formik.errors.name}</p>
             <Field className="campo_entrada" placeholder="Cantidad (gr/unidades)" name="quantity" type="number" />
             <Field className="campo_entrada" placeholder="Ubicacion (Bodega/Arsenal) --temporal" name="channel" as='select' >
               <option>Ingrese Ubicacion</option>
@@ -56,61 +57,3 @@ const CreateStockItem = () => {
 
 export default CreateStockItem
 
-/**
- * 
- * import React from 'react';
-import { Formik, Form } from 'formik';
-import { TextField } from './TextField';
-import * as Yup from 'yup';
-
-export const Signup = () => {
-  const validate = Yup.object({
-    firstName: Yup.string()
-      .max(15, 'Must be 15 characters or less')
-      .required('Required'),
-    lastName: Yup.string()
-      .max(20, 'Must be 20 characters or less')
-      .required('Required'),
-    email: Yup.string()
-      .email('Email is invalid')
-      .required('Email is required'),
-    password: Yup.string()
-      .min(6, 'Password must be at least 6 charaters')
-      .required('Password is required'),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Password must match')
-      .required('Confirm password is required'),
-  })
-  return (
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      }}
-      validationSchema={validate}
-      onSubmit={values => {
-        console.log(values)
-      }}
-    >
-      {formik => (
-        <div>
-          <h1 className="my-4 font-weight-bold .display-4">Sign Up</h1>
-          <Form>
-            <TextField label="First Name" name="firstName" type="text" />
-            <TextField label="last Name" name="lastName" type="text" />
-            <TextField label="Email" name="email" type="email" />
-            <TextField label="password" name="password" type="password" />
-            <TextField label="Confirm Password" name="confirmPassword" type="password" />
-            <button className="btn btn-dark mt-3" type="submit">Register</button>
-            <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
-          </Form>
-        </div>
-      )}
-    </Formik>
-  )
-}
-© 2022 GitHub, Inc.
- */
