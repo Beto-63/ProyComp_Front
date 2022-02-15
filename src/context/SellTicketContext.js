5668818c171228a1f1051fc0fcb7fe2e40fe1ad2
Alberto Isaza-PMP <87019347+Beto-63@users.noreply.github.com>
2022-02-15 18:27:58 -0500

    SellTicket Context v.Isaza

--- src/context/SellTicketContext.js
+++ src/context/SellTicketContext.js
@@ -4,47 +4,25 @@ import { createContext, useState } from "react";
 const SellTicketContext = createContext();
 
 //se crea el proveedor de contexto (es el que agrupa el arbol de elementos)
-const SellTicketProvider = ({children})=>{
+const SellTicketProvider = ({ children }) => {
+    const [origins, setOrigins] = useState([{}]);
+    const [paymentMethods, setPaymentMethods] = useState([{}]);
+    const [categories, setCategories] = useState([{}]);
+    const [selectedProducts, setSelectedProducts] = useState([{}])
+    const [client, setClient] = useState({})
 
-    
-    //Función para enviar petición al servidor
-    const handleClientRegister = async (objClient)=>{
-        //enviar los datos capturados a la base de datos
-        const resp = await fetch('https://dokotestback.herokuapp.com/client',{
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            //enviamos los datos por body y se debe convertir el objeto en JSON
-            body: JSON.stringify(objClient)
-        })
-        return resp;    
-    }
-    const getProductByCatName = async (objProduct) => {        
-        const resp = await fetch('https://dokotestback.herokuapp.com/product/findByCatName', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(objProduct)
-        })
-        return resp;
-    }
-
-    const [form, setForm] = useState()
-
-    const [product, setProduct] = useState([]);
-
-    const [selected, setSelected] = useState([]);
-
-    const [sellTicket, setSellTicket] = useState([])
-      
-    //aqui voy a crear el GET - hay que envolver toda la app con un Contexto global y empezar a hacer los contextos unitarios.
-    const data = {handleClientRegister, getProductByCatName, form, setForm, product, setProduct, selected, setSelected, sellTicket, setSellTicket };
+    const data = {
+        //handleClientRegister, //getProductByCatName,
+        origins, setOrigins,
+        paymentMethods, setPaymentMethods,
+        categories, setCategories,
+        selectedProducts, setSelectedProducts,
+        client, setClient
+    };
 
     return <SellTicketContext.Provider value={data}>{children}</SellTicketContext.Provider>
 }
 
 //se exportan el context y el provider
-export {SellTicketProvider};
+export { SellTicketProvider };
 export default SellTicketContext;
