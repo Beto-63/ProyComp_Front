aacd86b5ef1ca75c9debc7f7aa3ed1558e1288f9
Alberto Isaza-PMP <87019347+Beto-63@users.noreply.github.com>
2022-02-15 18:34:55 -0500

    update

--- src/components/sellTicket/ProductSelectTable.js
+++ src/components/sellTicket/ProductSelectTable.js
@@ -1,110 +1,78 @@
 import React, { useContext } from 'react';
-import { Table, Button, Form } from 'react-bootstrap';
+import { Table, Button } from 'react-bootstrap';
+import CashContext from '../../context/CashContext';
 import SellTicketContext from '../../context/SellTicketContext';
 
-
-let medioPago = [{
-    tipo: 'Crédito',
-    medio: 'Tarjeta de Crédito'
-}, {
-    tipo: 'Débito',
-    medio: 'Tarjeta Débito'
-}, {
-    tipo: 'Efectivo',
-    medio: 'Efectivo'
-}]
 const ProductSelectTable = () => {
 
-    const { selected, setSelected, sellTicket, setSellTicket } = useContext(SellTicketContext)
+    const { origins, paymentMethods } = useContext(SellTicketContext)
+    const { channel } = useContext(CashContext)
 
-    const handleSellTicket = () => {
-        let obj = {...selected, client_id: '', products_sold: '', amount_sold: '', channel_id: '', payment_method_id: '', user_id: '', status: '' }
-        //de esta forma se llena selected con el objeto pero se sobreescribe cada vez que seleccionamos un elemento
-        /* setSelected(obj) */
-        //de esta forma se guarda el array y se va llenando cada vez que seleccionamos un elemento
-        let array = [obj]
-        setSellTicket(obj)
-        console.log('sellticket: ')
-        console.log(sellTicket)
-    }
-
-
-    let suma = null
-
-    const handleRemove = (id) => {
-        const newSelected = selected.filter((select) => select.id !== id)
-        setSelected(newSelected)
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        handleSellTicket()
-        console.log('haciendo submit')
-    }
     return (
         <div>
+            { }
             <div>
-                <Form onSubmit={handleSubmit}>
-                    <Table striped bordered hover size="sm">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Producto</th>
-                                <th>Cantidad</th>
-                                <th>Precio</th>
-                                <th>Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {selected.map((element, i) => {
-                                return (
-                                    <tr key={i}>
-                                        <th>{i + 1}</th>
-                                        <td>{element.name}</td>
-                                        <td>{element.amount}</td>
-                                        <td>{element.price}</td>
-                                        <td>
-                                            <>
-                                                <div>
-                                                    <Button onClick={() => { handleRemove(element.id) }} variant="danger" size="sm" >
-                                                        Eliminar
-                                                    </Button>
-                                                </div>
-                                            </>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-
-                        </tbody>
-                    </Table>
-                
                 <Table striped bordered hover size="sm">
-                    <tbody>
-                        {selected.map(element => {
-                            if (element.price) {
-                                let multiplicacion = (element.amount * element.price)
-                                suma += multiplicacion
-                            }
-                        })}
+                    <thead>
                         <tr>
-                            <td>Total</td>
-
-                            <td>{suma}</td>
+                            <th>#</th>
+                            <th>Producto</th>
+                            <th>Qty</th>
+                            <th colSpan={2}>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Café en leche</td>
+                            <td>10</td>
+                            <td>5.000</td>
+                            <td>Eliminar</td>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td>Tinto Negro</td>
+                            <td>4</td>
+                            <td>4.000</td>
+                            <td>Eliminar</td>
                         </tr>
                     </tbody>
                 </Table>
+                <Table striped bordered hover size="sm">
+                    <tbody>
+                        <tr>
+                            <td>Total</td>
+                            <td>9.000</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+            <div>
                 <p>Medio de Pago</p>
                 <select>
                     <option value=''>Elija Medio de Pago</option>
-                    {medioPago.map(e => <option name='medio' value={e.tipo}>{e.medio}</option>)}
+                    {paymentMethods.map((e, index) => {
+                        return (
+                            <option key={index} value={e.name} >{e.name}</option>
+                        )
+                    })}
+
                 </select>
-                <br/>
-                <Button type="submit">Vender</Button>
-                </Form>
-            </div> 
+            </div>
+            <div>
+                <p>Origen de la venta</p>
+                <select>
+                    <option defaultValue={channel}>La venta se origina en:</option>
+                    {origins.map((e, index) => {
+                        return (
+                            <option key={index} value={e.name} >{e.name}</option>
+                        )
+                    })}
+
+                </select>
+            </div>
+            <Button type="submit">Vender</Button>
         </div>
-        
     );
 };
 
