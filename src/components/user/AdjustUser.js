71f657caf672667c8babb4215bc90d68ac0ad072
Andrés G <andres88884@hotmail.com>
2022-04-27 11:10:20 -0500

    fetch components/user

--- src/components/user/AdjustUser.js
+++ src/components/user/AdjustUser.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import { Row, Container } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 
 /**********************Importacion de Componentes**************************/
-import { server } from '../../context/Api'
+import { server } from '../../context/Api';
+import AuthContext from "../../context/AuthContext";
 
 /**********************Importacion de Estilos******************************/
-import '../generic/Light-bkg.css'
+import '../generic/Light-bkg.css';
 
 const schema = yup.object({
     /*El primero debe ser el tipo de dato y el ultimo debe ser el required*/
@@ -24,6 +25,10 @@ const schema = yup.object({
 });
 
 const AdjustUser = () => {
+
+    const { setAuth } = useContext(AuthContext);
+
+    let navigate = useNavigate();
 
     const objUser = {
         name: '',
@@ -54,8 +59,73 @@ const AdjustUser = () => {
             },
             body: JSON.stringify()
         })
-            .then(response => response.json())
-            .then(json => setUbicaciones(json));
+            //.then(response => response.json())
+            //.then(json => setUbicaciones(json));
+
+            .then(async (resp)=>{ // (primer .then)
+            
+                switch (resp.status) {
+        
+                    // Middleware - Inicio
+                    
+                    case 401:
+                        localStorage.removeItem("token");
+                        setAuth(false);
+                        window.alert("Intente autenticarse nuevamente");
+                        navigate("/login");
+                        break;
+        
+                    case 404:
+                        localStorage.removeItem("token");
+                        setAuth(false);
+                        window.alert("404");
+                        navigate("/login");
+                        break;
+        
+                    case 403:
+                        //localStorage.removeItem("token");
+                        //setAuth(false);
+                        window.alert("403");
+                        navigate("/");
+                        break;
+                    
+                    // Middleware - Fin
+        
+                    // Códigos del backend - Inicio
+        
+                    case 200:
+                        //localStorage.removeItem("token");
+                        //setAuth(false);
+        
+                        // Se retorna el json con la información
+                        let json = await resp.json();
+        
+                        setUbicaciones(json); // Recibo de información del backend en json (Segundo .then)
+                        //window.alert(json);
+                        //navigate("/");
+                        break;
+        
+                    case 500:
+                        //localStorage.removeItem("token");
+                        //setAuth(false);
+                        window.alert("500");
+                        navigate("/");
+                        break;
+        
+                    // Códigos del backend - Fin
+                
+                    default:
+                        localStorage.removeItem("token");
+                        setAuth(false);
+                        window.alert("default case");
+                        navigate("/");
+                        break;
+                }
+        
+            }).catch(error=>{
+                console.error(error);
+            })
+
     }, [])
 
     useEffect(() => {
@@ -67,8 +137,73 @@ const AdjustUser = () => {
             },
             body: JSON.stringify()
         })
-            .then(response => response.json())
-            .then(json => setRoles(json));
+            //.then(response => response.json())
+            //.then(json => setRoles(json));
+
+            .then(async (resp)=>{ // (primer .then)
+            
+                switch (resp.status) {
+        
+                    // Middleware - Inicio
+                    
+                    case 401:
+                        localStorage.removeItem("token");
+                        setAuth(false);
+                        window.alert("Intente autenticarse nuevamente");
+                        navigate("/login");
+                        break;
+        
+                    case 404:
+                        localStorage.removeItem("token");
+                        setAuth(false);
+                        window.alert("404");
+                        navigate("/login");
+                        break;
+        
+                    case 403:
+                        //localStorage.removeItem("token");
+                        //setAuth(false);
+                        window.alert("403");
+                        navigate("/");
+                        break;
+                    
+                    // Middleware - Fin
+        
+                    // Códigos del backend - Inicio
+        
+                    case 201:
+                        //localStorage.removeItem("token");
+                        //setAuth(false);
+        
+                        // Se retorna el json con la información
+                        let json = await resp.json();
+        
+                        setRoles(json); // Recibo de información del backend en json (Segundo .then)
+                        //window.alert(json);
+                        //navigate("/");
+                        break;
+        
+                    case 500:
+                        //localStorage.removeItem("token");
+                        //setAuth(false);
+                        window.alert("500");
+                        navigate("/");
+                        break;
+        
+                    // Códigos del backend - Fin
+                
+                    default:
+                        localStorage.removeItem("token");
+                        setAuth(false);
+                        window.alert("default case");
+                        navigate("/");
+                        break;
+                }
+        
+            }).catch(error=>{
+                console.error(error);
+            })
+
     }, [])
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm({
@@ -88,9 +223,79 @@ const AdjustUser = () => {
             },
             body: JSON.stringify(objUser)
         })
-            .then(response => response.json())
-            .then(json => setToEdit(json));
+            //.then(response => response.json())
+            //.then(json => setToEdit(json));
         //busca los datos del Usuario y los presenta en el formulario como en ajuste de producto
+
+        .then(async (resp)=>{ // (primer .then)
+            
+            switch (resp.status) {
+    
+                // Middleware - Inicio
+                
+                case 401:
+                    localStorage.removeItem("token");
+                    setAuth(false);
+                    window.alert("Intente autenticarse nuevamente");
+                    navigate("/login");
+                    break;
+    
+                case 404:
+                    localStorage.removeItem("token");
+                    setAuth(false);
+                    window.alert("404");
+                    navigate("/login");
+                    break;
+    
+                case 403:
+                    //localStorage.removeItem("token");
+                    //setAuth(false);
+                    window.alert("403");
+                    navigate("/");
+                    break;
+                
+                // Middleware - Fin
+    
+                // Códigos del backend - Inicio
+    
+                case 200:
+                    //localStorage.removeItem("token");
+                    //setAuth(false);
+    
+                    // Se retorna el json con la información
+                    let json = await resp.json();
+    
+                    setToEdit(json); // Recibo de información del backend en json (Segundo .then)
+                    //window.alert(json);
+                    //navigate("/");
+                    break;
+
+                case 400:
+                    window.alert("400: Usuario no encontrado");
+                    //navigate("/");
+                    break;
+
+                case 500:
+                    //localStorage.removeItem("token");
+                    //setAuth(false);
+                    window.alert("500");
+                    navigate("/");
+                    break;
+    
+                // Códigos del backend - Fin
+            
+                default:
+                    localStorage.removeItem("token");
+                    setAuth(false);
+                    window.alert("default case");
+                    navigate("/");
+                    break;
+            }
+    
+        }).catch(error=>{
+            console.error(error);
+        })
+
     }
 
     const onSubmit = (data) => {
